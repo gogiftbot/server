08ad03ca69dd42f889186c2411476170708f91e8
Denis Muraveiko <d.muraveiko@leenda.com>
2025-07-09 15:18:59 +0300

    release-2.0.21

---
+++ src/bin/bot.ts
@@ -0,0 +1,32 @@
+import cron from "node-cron";
+import { BotService } from "@/services/bot.service";
+import { marketplaceService } from "@/services/marketplace.service";
+import { CaseService } from "@/services/case.service";
+import { tonService } from "@/services/ton.service";
+
+import { context } from "./context";
+
+cron.schedule("0 * * * *", async () => {
+  try {
+    console.log(
+      "NFT_PRICE_CRON_JOB",
+      new Date().toLocaleDateString(),
+      new Date().toLocaleTimeString(),
+    );
+    await marketplaceService.updatePrices(context.prisma);
+
+    const data = await CaseService.analytics(context.prisma);
+    const alert_data = data.filter((item) => item.price < item.price_0_margin);
+    if (alert_data.length) {
+      await new BotService(context.prisma).casePriceAlert(alert_data);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+cron.schedule("* * * * *", async () => {
+  await tonService.onDepositTx(context.prisma);
+});
+
+new BotService(context.prisma, true).listen();
