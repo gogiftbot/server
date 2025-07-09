40e736a5e719e84f8ac7f0bf7c916561eb8c73e3
Denis Muraveiko <d.muraveiko@leenda.com>
2025-07-09 16:22:23 +0300

    release-2.0.22

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 
 import accountRouter from "./account.router";
+import authRouter from "./auth.router";
 import casesRouter from "./cases.router";
 import giftRouter from "./gift.router";
 import paymentRouter from "./payment.router";
@@ -9,6 +10,7 @@ const router = express.Router();
 
 export const initRouters = (context: Context): express.Router => {
   router.use("/account", accountRouter(context));
+  router.use("/auth", authRouter(context));
   router.use("/cases", casesRouter(context));
   router.use("/gift", giftRouter(context));
   router.use("/payment", paymentRouter(context));
