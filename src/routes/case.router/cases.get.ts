85d22ac0068b2dfb20975a1eac7235d188cefc59
Denis Muraveiko <d.muraveiko@leenda.com>
2025-08-01 23:23:48 +0300

    release-2.1.792

--- src/routes/case.router/cases.get.ts
+++ src/routes/case.router/cases.get.ts
@@ -43,7 +43,7 @@ export async function CasesGet(
     },
   });
 
-  const targetIndex = cases.findIndex((item) => item.title === "Westside");
+  const targetIndex = cases.findIndex((item) => item.title === "Ghetto");
 
   if (targetIndex !== -1) {
     const [targetItem] = cases.splice(targetIndex, 1);
